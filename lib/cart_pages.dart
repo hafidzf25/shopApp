aff352d020b87df2b00ff0b29a7d39e9bbf384ec
hafidzf25 <abdullahnestax@gmail.com>
2025-02-18 16:23:51 +0700

    Add Dialog on Cart

--- lib/cart_pages.dart
+++ lib/cart_pages.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:shop_app/global_variables.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/cart_provider.dart';
 
 class CartPages extends StatelessWidget {
   const CartPages({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final cart = Provider.of<CartProvider>(context).cart;
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -28,7 +30,44 @@ class CartPages extends StatelessWidget {
                 style: Theme.of(context).textTheme.bodySmall,
               ),
               subtitle: Text('Size: ${cartItem['sizes']}'),
-              trailing: IconButton(onPressed: () {}, icon: Icon(Icons.delete, color: Colors.red,)),
+              trailing: IconButton(
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          title: Text("Delete Product"),
+                          content:
+                              Text("Are you sure want to delete this product?"),
+                          actions: [
+                            TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text(
+                                  "No",
+                                  style: TextStyle(color: Colors.blue),
+                                )),
+                            TextButton(
+                                onPressed: () {
+                                  context
+                                      .read<CartProvider>()
+                                      .removeProduct(cartItem);
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text(
+                                  "Yes",
+                                  style: TextStyle(color: Colors.red),
+                                )),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                  icon: Icon(
+                    Icons.delete,
+                    color: Colors.red,
+                  )),
             );
           },
         ),
